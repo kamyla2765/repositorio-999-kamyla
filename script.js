1fb2b9058229dc77df5c6b60be5989c9d2fbe6f8
kamyla2765 <kamylasousa13@icloud.com>
2025-12-01 16:25:40 -0300

    primeiro commit da segunda

---
+++ script.js
@@ -0,0 +1,25 @@
+// let titulo = document.getElementById("titulo");
+// let botao = document.getElementById("botao");
+// let botaoClass = document.getElementsByClassName("titulo")
+// let css = document.querySelector("#titulo")
+// titulo.textcontent ="Novo Título!";
+// let container = document.getElementById("container");
+//  container.innerHTML = "<p>Novo Container</p>";
+
+// console.log(css);
+
+// titulo.style.color = "rgba(114, 148, 250, 1)";
+// titulo.style.fontSize = "24px";
+
+// titulo.classList.add("destaque");
+// titulo.classList.remove("antiga-classe");
+
+// let botao = document.getElementById("botao");
+// botao.classList.remove("buttonClass");
+// botao.classList.add("button-warning");
+
+// let button = document.getElementById("botao");
+// button.addEventListener("click", () => {
+//     alert("Botão clicado!");
+// });
+
